b927bd3b365a44dd8a12a9ab580c3037c69a0f5d
MyCode07 <erem19992015@mail.ru>
2024-10-31 18:04:17 +0300

    add sticky hero

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,9 @@ import "./parts/popup.js";
 import "./parts/menu.js";
 import "./parts/search.js";
 import { Fancybox } from "@fancyapps/ui";
+import { stickyHeader } from "./parts/header.js";
 
+stickyHeader();
 
 Fancybox.bind("[data-fancybox]", {
     Carousel: {
