d53b789f3ca519edf5167016476fe0406a15f6df
MyCode07 <erem19992015@mail.ru>
2024-12-03 12:33:47 +0300

    fix header scroll issue

--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -3,17 +3,18 @@ const header = document.querySelector('header');
 export const stickyHeader = () => {
     if (!header) return;
 
-    const headerheigth = header.getBoundingClientRect().height * 2;
+    const headerheigth = header.getBoundingClientRect().height;
 
     const sticky = () => {
-        if (window.scrollY > headerheigth) {
+        if (window.scrollY > 200) {
             header.classList.add('_sticky')
         }
         else {
             header.classList.remove('_sticky')
         }
-
+        document.querySelector('.wrapper').style.paddingTop = headerheigth + 'px';
     }
+
 
     sticky();
     window.addEventListener('scroll', sticky);
